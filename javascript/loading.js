d09d33584673fadb1eca1b1d4cdf94fea0dd36b8
Valerie Pham <111666452+Valerie-Ha@users.noreply.github.com>
2023-03-03 00:03:10 +0000

    wEHEEEEEE

---
+++ javascript/loading.js
@@ -0,0 +1,17 @@
+const bg = document.querySelector(".bg")
+const number = document.querySelector(".loading-text")
+
+let i = 0
+let not_var = setInterval(loading, 50)
+
+function loading() {
+    i++
+    number.style.opacity = `${1-i/100}`
+    bg.style.filter = `blur(${30-0.3*i}px)`
+    number.innerText = `${i}%`
+    if(i == 100) {
+        clearInterval(not_var)
+    }
+}
+
+
