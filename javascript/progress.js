a9518a1c65b606dafd59db812c7d2fd5a310904a
Valerie Pham <111666452+Valerie-Ha@users.noreply.github.com>
2023-03-08 17:31:48 +0000

    Wed, Mar 8, 2023, 5:31 PM +00:00

--- javascript/progress.js
+++ javascript/progress.js
@@ -5,10 +5,27 @@ const circles = documenr.querySelectorAll('.circle')
 
 let currentActive = 1
 
+
 next.addEventListener('click', () => {
     currentActive++
 
     if (currentActive > 4) {
         currentActive = 4
     }
+
+    update()
 })
+
+prev.addEventListener('click', () => {
+    curentActive--
+
+    if (currentActive < 1) {
+        currentActive = 1
+    }
+
+    update()
+})
+
+function update() {
+    progress.style.width  = '${currentActive}'
+}
