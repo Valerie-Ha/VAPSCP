2e9a62cf641d9f4c31ad3bade7311aabb968b7c6
Valerie Pham <111666452+Valerie-Ha@users.noreply.github.com>
2023-03-20 17:04:05 +0000

    Mon, Mar 20, 2023, 5:04 PM +00:00

---
+++ javascript/jokes.js
@@ -0,0 +1,13 @@
+const jokeEL = document.getElementById('joke')
+const jokeBTN = document.getElementById('jokeBTN')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+async function generateJoke(){
+    const res = await fetch('https://icanhazdadjoke.com', {
+        headers: {Accept: 'application/json',},
+    })
+
+    const data = await res.json()
+    jokeEL.innerHTML = data.joke
+}
