2b95f5a26ddbe8d59d76a506cf47cc9e3133010e
Valerie Pham <111666452+Valerie-Ha@users.noreply.github.com>
2023-03-22 17:18:02 +0000

    Wed, Mar 22, 2023, 5:18 PM +00:00

--- javascript/colorboard.js
+++ javascript/colorboard.js
@@ -2,7 +2,7 @@ const container = document.getElementById('container')
 const colors = ['#eddd00', '#00d1ed', '#e3197b', '#ff9c08', '#0862ff' ]
 const SQUARES = 500
 
-for(let i = 0; 1 < SQUARES; i++) {
+for(let i = 0; i < SQUARES; i++) {
     const square = document.createElement('div')
     square.classList.add('square')
     container.appendChild(square)
