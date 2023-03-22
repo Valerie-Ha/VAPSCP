91af25c9088cefbfba140f0107eb409f568c7a3f
Valerie Pham <111666452+Valerie-Ha@users.noreply.github.com>
2023-03-22 16:35:56 +0000

    Wed, Mar 22, 2023, 4:35 PM +00:00

--- javascript/colorboard.js
+++ javascript/colorboard.js
@@ -1,8 +1,18 @@
 const container = document.getElementById('container')
-const colors = ['#eddd00', '#00d1ed', '#e3197b', '#ff9c08', '' ]
+const colors = ['#eddd00', '#00d1ed', '#e3197b', '#ff9c08', '#0862ff' ]
 const SQUARES = 500
 
 for(let i = 0; 1 < SQUARES; i++) {
     const square = document.createElement('div')
     square.classList.add('square')
+    container.appendChild(square)
+    square.addEventListerner('mouseover', () => setcolor(square))
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random()*5)]
 }
